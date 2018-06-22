a25f12e86f439af442590aed9f9c1df03881dc54
RetoqueFactoria <40504402+RetoqueFactoria@users.noreply.github.com>
2018-06-22 19:02:07 -0300

    Add files via upload

---
+++ Eliminar Canales Alpha.jsx
@@ -0,0 +1,36 @@
+//
+// RemoveAlphaChannels
+//   Removes all of the alpha channels from a document
+//
+// $Id$
+// Copyright: (c)2007, xbytor
+// License: http://www.opensource.org/licenses/bsd-license.php
+// Contact: xbytor@gmail.com
+//
+function main() {
+  if (app.documents.length == 0) {
+    return;
+  }
+
+  var doc = app.activeDocument;
+
+  var channels = doc.channels;
+  var alphas = [];
+  for (var i = 0; i < channels.length; i++) {
+    var channel = channels[i];
+    if (channel.kind == ChannelType.COMPONENT) {
+      continue;
+    }
+    alphas.push(channel);
+  }
+  while (alphas.length) {
+    var channel = alphas.pop();
+    channel.remove();
+  }
+};
+
+main();
+
+"RemoveAlphaChannels.jsx";
+// EOF
+
