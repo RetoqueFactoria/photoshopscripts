c1e82e5eb54ca0c4eabca50d89a4cd5273d92a7e
RetoqueFactoria <40504402+RetoqueFactoria@users.noreply.github.com>
2018-06-27 14:11:39 -0300

    Update ES Eliminar Canales Alpha.jsx

--- ES Eliminar Canales Alpha.jsx
+++ ES Eliminar Canales Alpha.jsx
@@ -1,12 +1,15 @@
-//
 // RemoveAlphaChannels
 //   Removes all of the alpha channels from a document
-//
 // $Id$
 // Copyright: (c)2007, xbytor
 // License: http://www.opensource.org/licenses/bsd-license.php
 // Contact: xbytor@gmail.com
-//
+// ============================================================================
+// Traducción: Juan Manuel Díaz
+// Sitio Web: http://www.retoquefactoria.com
+// Contacto: contacto@retoquefactoria.com
+// ============================================================================
+
 function main() {
   if (app.documents.length == 0) {
     return;
