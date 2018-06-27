783c0ac31c42626c0af3728a35592da4180f57e8
RetoqueFactoria <40504402+RetoqueFactoria@users.noreply.github.com>
2018-06-27 14:12:19 -0300

    Update and rename Editor de Nombres de Capa.jsx to ES Editor de Nombres de Capa.jsx

--- ES Editor de Nombres de Capa.jsx
+++ ES Editor de Nombres de Capa.jsx
@@ -1,4 +1,4 @@
-﻿// Editor de Nombres de Capa
+// Editor de Nombres de Capa
 // Copyright: ps-bridge-scripts@talktalk.net
 // Sitio Web: http://www.ps-bridge-scripts.talktalk.net
 // License: http://www.opensource.org/licenses/bsd-license.php
